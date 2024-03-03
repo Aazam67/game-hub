076b4ed5cc6c2806518a27fa3ce47f6f3c5d6e79
azam abasgholizade <azam8788@gmail.com>
2024-03-03 19:43:30 +0300

    Install chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+import App from "./App.tsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+//explane about imports: import Chakra provider add it after React import statements.
+//First we import stuff from React, then third party libraries, and finally our own import statements. then chakra,...
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
