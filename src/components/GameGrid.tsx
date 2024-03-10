19e6774ba45c5072ec112e0232c6d1ae042218f4
azam abasgholizade <azam8788@gmail.com>
2024-03-10 20:16:49 +0300

    Displaying platform icons

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,27 +1,6 @@
-import { useEffect, useState } from "react";
-import apiClient from "../services/api-client";
-
-interface Game {
-  id: number;
-  name: string;
-}
-
-interface FetchGamesResponse {
-  count: number;
-  results: Game[];
-}
-
+import useGames from "../hooks/useGames";
 const GameGrid = () => {
-  const [games, setGames] = useState<Game[]>([]);
-  const [error, setError] = useState("");
-
-  useEffect(() => {
-    apiClient
-      .get<FetchGamesResponse>("/games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.messages));
-  });
-
+  const { games, error } = useGames();
   return (
     <ul>
       {error && <text>{error}</text>}
