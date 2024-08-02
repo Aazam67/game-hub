012f7e6e982273d3c683a06f37ef5902398471c2
Aazam AB <azam8788@gmail.com>
2024-08-02 23:46:22 +0200

    Install Chakra

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,12 @@
 import { HStack, Image } from "@chakra-ui/react";
-import logo from "../assets/loogo.webp";
+import loggo from "../assets/loggo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 
 const NavBar = () => {
   //Webp images are highly optimized images for web.
   return (
     <HStack justifyContent="space-between" padding="10px">
-      <Image src={logo} boxSize="60px"></Image>
+      <Image src={loggo} boxSize="60px"></Image>
       <ColorModeSwitch />
     </HStack>
   );
